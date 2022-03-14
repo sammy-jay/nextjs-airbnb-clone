a2d925dfbb67c93e556a75edd97ac14629e7dd15
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 09:56:55 +0100

    2 files modified, 1 added

---
+++ components/Mediumcard.js
@@ -0,0 +1,5 @@
+const Mediumcard = ({ item: { img, title } }) => {
+  return <div>card</div>;
+};
+
+export default Mediumcard;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import Mediumcard from '../components/Mediumcard';
 import Smallcard from '../components/Smallcard';
 
 export default function Home({ exploreData }) {
@@ -19,9 +20,19 @@ export default function Home({ exploreData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           {/* Pull some data from the server */}
-          {exploreData?.map((item) => (
+         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+         {exploreData?.map((item) => (
             <Smallcard key={item.img} item={item} />
           ))}
+         </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-5">Live Anywhere</h2>
+          <div>
+          {cardsData?.map((item) => (
+            <Mediumcard key={item.img} item={item} />
+          ))}
+          </div>
         </section>
       </main>
     </div>
@@ -32,8 +43,10 @@ export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp').then(
     (res) => res.json()
   );
-
+  const cardsData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
   return {
-    props: { exploreData },
+    props: { exploreData,cardsData },
   };
 }
