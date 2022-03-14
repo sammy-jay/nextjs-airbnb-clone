fe44a25f65d59085932a564718843c70b6edd7ac
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 09:13:34 +0100

    1 file modified

--- components/Smallcard.js
+++ components/Smallcard.js
@@ -1,5 +1,17 @@
-const Smallcard = ({ item: { image, location, distance } }) => {
-  return <div>Small component</div>;
+import Image from 'next/image';
+
+const Smallcard = ({ item: { img, location, distance } }) => {
+  return (
+    <div className="flex items-center">
+      <div className="relative h-16 w-16">
+        <Image src={img} layout="fill" className="rounded-lg" />
+      </div>
+      <div>
+        <h2>{location}</h2>
+        <h2>{distance}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Smallcard;
