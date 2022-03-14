5326ca7b3086d6ec07c5e1c5db62c3d4952a7d0a
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 07:54:54 +0100

    1 file modified, 1 added

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,22 @@
+import Image from 'next/image';
 const Header = () => {
-  return <header>
-    h
-  </header>;
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+      {/* Left */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      {/* Middle */}
+      <div></div>
+      {/* Right */}
+      <div></div>
+    </header>
+  );
 };
 
 export default Header;
