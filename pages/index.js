a5909e7300796bce41247755c639e489470d763a
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 08:59:05 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,15 @@ export default function Home() {
       {/* Banner */}
       <Banner />
 
-      <main className="max-w-7xl mx-auto ">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6 ">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from the server */}
         </section>
       </main>
     </div>
   );
 }
+
+export async function getStaticProps() {}
