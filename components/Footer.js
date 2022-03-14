5d46ceed531df76ce9b17c2d69cf6d16baa91f27
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 12:47:37 +0100

    1 file modified, 1 added

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awewsome clone</p>
+        <p>Referral accepted</p>
+        <p>Smokey Replubic</p>
+      </div>
+
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>pure_smoke</p>
+        <p>Presents</p>
+        <p>Getting a job</p>
+        <p>Hundreds of oppurtunities</p>
+        <p>Begin job search</p>
+      </div>
+
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi to YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Largecard from '../components/Largecard';
 import Mediumcard from '../components/Mediumcard';
@@ -42,6 +43,7 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   );
 }
