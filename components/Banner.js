bcade7cffb3a98ae186ad17d39d77850ccd5d5a7
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 08:36:33 +0100

    1 file modified

--- components/Banner.js
+++ components/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure whre to go? Perfect.</p>
-        <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-xl">
+        <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
           I'm flexible
         </button>
       </div>
