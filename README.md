0719db5670f8fc4b4460358c1037abc7a095c713
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 07:16:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-adx1sx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-adx1sx)
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AirBNB Clone</title>
+      </Head>
+      <h2 className="text-blue-600">Hello</h2>
+    </div>
+  );
+}
