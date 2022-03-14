5e73fffaf550d2e43f515e7c763563eb8ce41973
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 12:36:48 +0100

    1 file modified

--- components/Largecard.js
+++ components/Largecard.js
@@ -10,7 +10,14 @@ const Largecard = ({ img, title, description, buttonText }) => {
           className="rounded-2xl"
         />
       </div>
-      <div></div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl mb-3 w-64">{title}</h3>
+        <p>{description}</p>
+
+        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
+          {buttonText}
+        </button>
+      </div>
     </section>
   );
 };
